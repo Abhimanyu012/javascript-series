0ecfe72628b9f47634acac0b4ce974b7f7c0aec0
Abhimanyu <90468609+Abhimanyu012@users.noreply.github.com>
2024-04-03 09:59:55 +0000

    variable learn

---
+++ basics01/01variables.js
@@ -0,0 +1,13 @@
+const accountId = 1444
+let accountEmail = "abhimanyukumarssm0012@gmail.com"
+var accountPassword = "123456"
+accountXity = "Delhi"
+
+// accountId = 2// it not allowed because of const
+
+console.log(accountId);
+
+accountEmail ="amancul100@gmail.com"
+
+accountPassword ="321654"
+ console.table([ accountEmail, accountId, accountPassword,]);
