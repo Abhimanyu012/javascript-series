d76387034386ae3eeba6f0398534a1900b466325
Abhimanyu <90468609+Abhimanyu012@users.noreply.github.com>
2024-04-03 10:05:36 +0000

    var not use

--- basics01/01variables.js
+++ basics01/01variables.js
@@ -4,6 +4,7 @@ var accountPassword = "123456"
 accountXity = "Delhi"
 
 // accountId = 2// it not allowed because of const
+//  prefer not to use var because of issue in block scope ans functional 
 
 console.log(accountId);
 
